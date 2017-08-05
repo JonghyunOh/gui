2c6a510bc5dbf0c3158751958693b4c66a1bac01
JonghyunOh <korzakk@gmail.com>
2017-08-04 20:10:51 -0400

    modified metric/Imperial converting part

--- BMICalculatorJack/BMICalculatorJack/BMICalc.Designer.cs
+++ BMICalculatorJack/BMICalculatorJack/BMICalc.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BMICalculatorJack
 {
-    partial class Form1
+    partial class BMICalc
     {
         /// <summary>
         /// Required designer variable.
@@ -36,6 +36,8 @@
             this.ImperialRadioBtn = new System.Windows.Forms.RadioButton();
             this.heightBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.HeightUnit = new System.Windows.Forms.Label();
+            this.WeightUnit = new System.Windows.Forms.Label();
             this.resultLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.resultBox = new System.Windows.Forms.TextBox();
@@ -54,6 +56,8 @@
             this.tableLayoutPanel1.Controls.Add(this.ImperialRadioBtn, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.heightBox, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.HeightUnit, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.WeightUnit, 1, 4);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 41);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 7;
@@ -77,6 +81,7 @@
             this.metricRadioBtn.TabStop = true;
             this.metricRadioBtn.Text = "Metric";
             this.metricRadioBtn.UseVisualStyleBackColor = true;
+            this.metricRadioBtn.CheckedChanged += new System.EventHandler(this.metricRadioBtn_CheckedChanged);
             // 
             // label2
             // 
@@ -104,7 +109,7 @@
             this.submitBtn.TabIndex = 6;
             this.submitBtn.Text = "Calculate BMI";
             this.submitBtn.UseVisualStyleBackColor = true;
-            this.submitBtn.Click += new System.EventHandler(this.button1_Click);
+            this.submitBtn.Click += new System.EventHandler(this.submitButton_Click);
             // 
             // ImperialRadioBtn
             // 
@@ -116,6 +121,7 @@
             this.ImperialRadioBtn.TabStop = true;
             this.ImperialRadioBtn.Text = "Imperial";
             this.ImperialRadioBtn.UseVisualStyleBackColor = true;
+            this.ImperialRadioBtn.CheckedChanged += new System.EventHandler(this.ImperialRadioBtn_CheckedChanged);
             // 
             // heightBox
             // 
@@ -133,6 +139,26 @@
             this.label4.Size = new System.Drawing.Size(100, 25);
             this.label4.TabIndex = 2;
             this.label4.Text = "My Height";
+            // 
+            // HeightUnit
+            // 
+            this.HeightUnit.AutoSize = true;
+            this.HeightUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightUnit.Location = new System.Drawing.Point(154, 87);
+            this.HeightUnit.Name = "HeightUnit";
+            this.HeightUnit.Size = new System.Drawing.Size(37, 25);
+            this.HeightUnit.TabIndex = 7;
+            this.HeightUnit.Text = "FT";
+            // 
+            // WeightUnit
+            // 
+            this.WeightUnit.AutoSize = true;
+            this.WeightUnit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WeightUnit.Location = new System.Drawing.Point(154, 169);
+            this.WeightUnit.Name = "WeightUnit";
+            this.WeightUnit.Size = new System.Drawing.Size(79, 25);
+            this.WeightUnit.TabIndex = 7;
+            this.WeightUnit.Text = "Pounds";
             // 
             // resultLabel
             // 
@@ -164,7 +190,7 @@
             this.resultBox.Size = new System.Drawing.Size(260, 82);
             this.resultBox.TabIndex = 4;
             // 
-            // Form1
+            // BMICalc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -178,9 +204,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.MaximizeBox = false;
-            this.Name = "Form1";
+            this.Name = "BMICalc";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "BMI Calculater by Jack";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -201,6 +227,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Label HeightUnit;
+        private System.Windows.Forms.Label WeightUnit;
     }
 }
 
---
+++ BMICalculatorJack/BMICalculatorJack/BMICalc.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMICalculatorJack
+{
+    public partial class BMICalc : Form
+    {
+        public BMICalc()
+        {
+            InitializeComponent();
+            this.ImperialRadioBtn.Checked = true;
+            this.metricRadioBtn.Checked = false;
+            this.resultBox.Text = "Hi!";
+        }
+
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            float height = 0.0f;
+            float weight = 0.0f;
+            float result = 0.0f;
+            
+
+            Boolean unitFlag = this.ImperialRadioBtn.Checked;
+
+            Boolean valid = ValidationNumbers(out height, out weight);
+
+            if (valid)
+            {
+                result = calculation(unitFlag, height, weight);
+                displayResult(result);
+            }
+            
+        }
+
+
+
+        private Boolean ValidationNumbers(out float height, out float weight)
+        {
+            Boolean result = false;
+
+            try
+            {
+                height = float.Parse(this.heightBox.Text);
+                weight = float.Parse(this.weightBox.Text);
+                result = true;
+            }
+            catch(Exception e)
+            {
+                height = 0.0f;
+                weight = 0.0f;
+                this.heightBox.Text = "";
+                this.weightBox.Text = "";
+                this.resultBox.Text = e.Message;
+            }
+
+            return result;
+
+        }
+
+
+
+        private float calculation(Boolean unitFlag, float pheight, float pweight)
+        {
+            float result = 0.0f;
+
+            try
+            {
+                if (unitFlag)
+                {
+                    //Imperial
+                    result = (pweight * 703) / ((pheight * 12) * (pheight * 12));
+                }
+                else
+                {
+                    //Metric
+                    result = (pweight) / ((pheight / 100) * (pheight / 100));
+                }
+
+            }catch(Exception e)
+            {
+                this.resultBox.Text = e.Message;
+            }
+
+            return result;
+            
+        }
+
+
+
+        private void displayResult(float result)
+        {
+
+            string resultStr = "";
+            if (result >= 30.0f)
+            {
+                resultStr = "Obese";
+                this.resultBox.ForeColor = Color.Red;
+            }
+            else if (result >= 25.0f && result <= 29.9)
+            {
+                resultStr = "Overwieight";
+                this.resultBox.ForeColor = Color.Orange;
+            }
+            else if (result >= 18.9f && result <= 29.9)
+            {
+                resultStr = "Normal";
+                this.resultBox.ForeColor = Color.Green;
+            }
+            else
+            {
+                resultStr = "Underweight";
+                this.resultBox.ForeColor = Color.Gray;
+            }
+
+            this.resultBox.Text = "your figure is " + result + " and " + resultStr;
+        }
+
+        private void metricRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            float hBox = 0.0f;
+            float wBox = 0.0f;
+
+            Boolean valid = ValidationNumbers(out hBox, out wBox);
+
+            if (valid && this.metricRadioBtn.Checked)
+            {
+                string temp = "" + (hBox / 0.0328);
+                if(temp.Length > 8)
+                {
+                    temp = temp.Substring(0, 8);
+                }               
+                this.heightBox.Text = temp;
+
+                temp = "" + (wBox / 2.20462);
+                if (temp.Length > 8)
+                {
+                    temp = temp.Substring(0, 8);
+                }
+                this.weightBox.Text = temp ;
+            }
+        }
+
+        private void ImperialRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            float hBox = 0.0f;
+            float wBox = 0.0f;  
+
+            Boolean valid = ValidationNumbers(out hBox, out wBox);
+
+            if (valid && this.ImperialRadioBtn.Checked)
+            {
+                string temp = "" + (hBox * 0.0328);
+                if (temp.Length > 8)
+                {
+                    temp = temp.Substring(0, 8);
+                }
+                this.heightBox.Text = temp;
+
+                temp = "" + (wBox * 2.20462);
+                if (temp.Length > 8)
+                {
+                    temp = temp.Substring(0, 8);
+                }
+                this.weightBox.Text = temp;
+            }
+        }
+    }
+}
